0ab8e339ca55201bd55ea5729c98b55447b6b6d6
JD <janelle.m.deane@gmail.com>
2020-09-30 09:37:06 -0700

    added basic questions and structure

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
+// Required variables
+var inquirer=require("inquirer")
+var fs =require("fs");
+
+inquirer
+.prompt([
+    {
+        type: "input",
+        message: "What is the Title?",
+        name: "title"
+      },
+      {
+        type: "input",
+        message: "Description",
+        name: "description",
+      },
+      {
+        type: "checkbox",
+        message: "What is included in your Table of Contents?",
+        name: "contents",
+        choices: ["Phone", "Email", "Snailmail"]
+      },
+      {
+        type: "input",
+        message: "Installation Instructions",
+        name: "installation",
+      },
+      {
+        type: "input",
+        message: "Usage?",
+        name: "usage",
+      },
+      {
+        type: "list",
+        message: "What is the License",
+        name: "license",
+        choices: ["MIT", ]
+      },
+      {
+        type: "input",
+        message: "Who are the contributors?",
+        name: "contribution",
+      },
+      {
+        type: "input",
+        message: "Tests",
+        name: "tests",
+      },
+      {
+        type: "input",
+        message: "Questions",
+        name: "questions",
+      },
+    ])
+.then(function (response) {
+    console.log(response)
+    fs.writeFile(`${response.title}.md`, JSON.stringify(response, null, 2), function(err){
+        if(err){
+           throw console.log(err)
+        }
+        console.log("Success")
+    })
+
+})
+
 // function that creates a readme file
 // run in node.js
 
@@ -10,3 +75,4 @@
 // License (list of options), 
 // Contributing, 
 // Tests and Questions
+// Install inquirer and fs packages 
