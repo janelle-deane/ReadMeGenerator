06b5ef063726403f4d713030f0aef170d1c610e2
JD <janelle.m.deane@gmail.com>
2020-09-30 08:52:03 -0700

    psuedo code

---
+++ index.js
@@ -0,0 +1,12 @@
+// function that creates a readme file
+// run in node.js
+
+// Generate a Title
+
+// Description
+// Table of Contents (checkbox),
+// Installation, 
+// Usage, 
+// License (list of options), 
+// Contributing, 
+// Tests and Questions
